0588b42c5dd6fcb65eefb0db49064fdcb923fa7a
Nikesh Ulak <cloudnikesh@gmail.com>
2018-05-18 14:39:32 +0545

    Starter wordpress theme with working condition.

---
+++ Gruntfile.js
@@ -0,0 +1,70 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    // COMPASS
+    compass: {
+        main: {
+          options: { // Target options
+            sassDir: './assets/src/scss/',
+            cssDir: './assets/build/css/',
+            environment: 'development',
+            outputStyle: 'compact',
+            sourcemap: true
+          }
+        } 
+    },
+
+    // COPY CONTENT
+    copy: {
+            js: {
+                files: [{
+                    expand: true,
+                    cwd: './assets/src/js',
+                    src: '**',
+                    dest: './assets/build/js/',
+                    filter: 'isFile'
+                }]
+            }, 
+            css: {
+                files: [{
+                    expand: true,
+                    cwd: './assets/src/css/',
+                    src: '**',
+                    dest: './assets/build/css/',
+                    filter: 'isFile'
+                }]
+            }, 
+    },
+
+    // GRUNT WATCH
+    watch: {
+        sass: {
+            files: [
+                './assets/src/scss/*.scss',
+                './assets/src/scss/**/*.scss',
+                '../assets/src/scss/**/**/*.scss'
+            ],
+            tasks: ['css']
+        },
+
+    }
+
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  // grunt.loadNpmTasks('grunt-contrib-concat');
+  // grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task(s).
+  grunt.registerTask('css', ['compass:main']);
+  // grunt.registerTask('js', ['concat']);
+  // grunt.registerTask('img', ['copy']); 
+  grunt.registerTask('default', ['compass', 'copy']);//, 'uglify', 'concat'
+
+};
---
+++ footer.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package st-blog
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer">
+
+		<div class="container">
+			<div class="st-blog-main-footer">
+				<div class="row">
+					<div class="col">
+						<?php dynamic_sidebar( 'footer-1' ); ?>
+					</div>
+					<div class="col">
+						<?php dynamic_sidebar( 'footer-2' ); ?>
+					</div>
+					<div class="col">
+						<?php dynamic_sidebar( 'footer-3' ); ?>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'st-blog' ) ); ?>">
+				<?php
+				/* translators: %s: CMS name, i.e. WordPress. */
+				printf( esc_html__( 'Proudly powered by %s', 'st-blog' ), 'WordPress' );
+				?>
+			</a>
+			<span class="sep"> | </span>
+				<?php
+				/* translators: 1: Theme name, 2: Theme author. */
+				printf( esc_html__( 'Theme: %1$s by %2$s.', 'st-blog' ), 'st-blog', '<a href="http://underscores.me/">Underscores.me</a>' );
+				?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
