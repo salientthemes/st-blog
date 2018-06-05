7a2c77c06556070547af5322a2c435ab4449fca6
Nikesh Ulak <cloudnikesh@gmail.com>
2018-06-05 16:09:16 +0545

    link, sidebar css, social icons markup.

--- template-parts/markup/social-icons.php
+++ template-parts/markup/social-icons.php
@@ -1,10 +1,19 @@
 <section id="st-blog-social-icons" class="mb-5">
 	<div class="container site-content">
 		<?php
-		wp_nav_menu( array(
-			'theme_location' => 'menu-2',
-			'menu_id'        => 'social-nav',
-		) );
+		// wp_nav_menu( array(
+		// 	'theme_location' => 'menu-2',
+		// 	'menu_id'        => 'social-nav',
+		// ) );
 		?>
+
+		<div class="menu-social-nav-container">
+			<ul id="social-nav" class="menu">
+				<li><a href="https://facebook.com">Facebook</a></li>
+				<li><a href="https://twitter.com">Twitter</a></li>
+				<li><a href="https://instagram.com">Instagram</a></li>
+				<li><a href="https://pinterest.com">Pinterest</a></li>
+			</ul>
+		</div>
 	</div>
 </section>
