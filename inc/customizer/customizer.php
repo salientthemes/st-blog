f2d76b04b0bac71e66bb2bb46a743b1b847fde52
Nikesh Ulak <cloudnikesh@gmail.com>
2018-06-08 14:10:59 +0545

    jquery removed from function php and style renamed as editor style.

--- inc/customizer/customizer.php
+++ inc/customizer/customizer.php
@@ -176,7 +176,7 @@ add_filter( 'salient_customizer_panels_sections_settings', 'st_blog_add_panels_s
  * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
  */
 function st_blog_customize_preview_js() {
-    wp_enqueue_script( 'st-blog-customizer', get_template_directory_uri() . '/assets/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+    wp_enqueue_script( 'st-blog-customizer', get_template_directory_uri() . '/assets/build/js/customizer.js', array( 'customize-preview' ), '20151215', true );
 }
 add_action( 'customize_preview_init', 'st_blog_customize_preview_js' );
 
