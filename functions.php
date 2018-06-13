9fa6c00533eefea28a4b31f37b130c2d95fe86d5
kabiraj lamichhane <cloudkabiraj@gmial.com>
2018-06-13 11:31:57 +0545

    trt pro st blog added

--- functions.php
+++ functions.php
@@ -258,6 +258,9 @@ require get_template_directory() . '/inc/custom-header.php';
  */
 require get_template_directory() . '/inc/template-tags.php';
 
+/*update to pro added*/
+require_once( trailingslashit( get_template_directory() ) . 'trt-customize-pro/st-blog/class-customize.php' );
+
 
 /**
  * Load Jetpack compatibility file.
