4824133e82b77fcac9ccfd7e71ca9ed9de381f9c
Nikesh Ulak <cloudnikesh@gmail.com>
2018-07-31 14:31:03 +0545

    Entry meta css.

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -47,7 +47,7 @@ if ( ! function_exists( 'st_blog_posted_by' ) ) :
 		
 			$byline = sprintf(
 				/* translators: %s: post author. */
-				esc_html_x( 'By %s | ', 'post author', 'st-blog' ),
+				esc_html_x( 'By %s', 'post author', 'st-blog' ),
 				'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
 			);
 
